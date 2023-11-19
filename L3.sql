8af973dffee0bddde610b468520dd4cb19ed4a50
mint trees <104205313+minttreesdao@users.noreply.github.com>
2023-11-19 07:31:47 +0300

    Update L3.sql
    + user analysis

--- L3.sql
+++ L3.sql
@@ -26,6 +26,15 @@ FROM intellilens.L3_interaction i
 GROUP BY i.profile_id, year_month, i.interaction_type
 ;
 
+CREATE OR REPLACE TABLE intellilens.L3_publication_year_month AS
+SELECT
+  p.profile_id
+, SUBSTRING(CAST(p.publication_date AS STRING), 1, 7) AS year_month
+, p.publication_type
+, count(*) AS number_publication
+FROM intellilens.L3_publication p
+GROUP BY p.profile_id, year_month, p.publication_type
+;
 
 CREATE OR REPLACE TABLE intellilens.L3_publication_year_month AS
 SELECT
@@ -35,4 +44,31 @@ SELECT
 , count(*) AS number_publication
 FROM intellilens.L3_publication p
 GROUP BY p.profile_id, year_month, p.publication_type
+;
+
+CREATE OR REPLACE TABLE intellilens.L3_user_analysis AS
+SELECT 
+  i.interaction_profile_id AS user_id
+, p.profile_name
+, i.profile_id
+, MIN(interaction_date) AS first_interaction_date
+, MAX(interaction_date) AS last_interaction_date
+, COUNT(DISTINCT CONCAT(p.profile_id, '_', interaction_type)) AS number_interaction
+, CASE WHEN interaction_type IN ('COMMENT', 'MIRROR', 'PAID COLLECT', 'FREE COLLECT', 'QUOTE', 'FOLLOW') THEN 'high_value'
+  ELSE 'low_value' END AS interaction_category
+, SUM(CASE WHEN interaction_type = 'FOLLOW' THEN 111
+       WHEN interaction_type = 'PAID COLLECT' THEN 100
+       WHEN interaction_type = 'COMMENT' THEN 60 
+       WHEN interaction_type = 'QUOTE' THEN 45
+       WHEN interaction_type = 'FREE COLLECT' THEN 40 
+       WHEN interaction_type = 'MIRROR' THEN 35 
+       WHEN interaction_type = 'MENTION' THEN 15 
+       WHEN interaction_type = 'UPVOTE' THEN 15 
+       ELSE 0 END) AS user_score
+, SUM(amount_USD) AS amount_USD
+FROM `intellilens.L3_interaction` i
+LEFT JOIN `intellilens.L3_profile` p
+  ON p.profile_id = i.interaction_profile_id
+WHERE p.profile_name IS NOT NULL
+GROUP BY i.interaction_profile_id, profile_id, interaction_category, profile_name
 ;
